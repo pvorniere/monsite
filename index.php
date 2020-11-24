4cc8b70ae1b89d27a541639b31cc50e30e3c02f6
VORNIERE <73873976+pvorniere@users.noreply.github.com>
2020-11-24 09:29:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,444 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Paul VORNIERE's website</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/form.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!--
+displayMenu($menu)
+displayCompetences($competences) -->
+
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="monsite.html">VORNIERE Paul</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#index">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">A propos de moi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#skills">Compétences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#experience">Expériences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#formation">Formation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-pv.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1 id="index">Paul VORNIERE</h1>
+            <span class="subheading">Fait tes projets en silence, la réussite se chargera du bruit.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <nav class="menu-flottant">
+    <ul>
+      <li><span><i class="fas fa-envelope"></i></span><a href="mailto:vorniere.paul@gmail.com">Mail</a></li>
+      <li><span><i class="fas fa-phone"></i></i></span><a href="tel:0781982425">Téléphone</a></li>
+      <li><span><i class="fab fa-linkedin-in"></i></span><a href="https://www.linkedin.com/in/paul-vorniere-46166b1b9/">LinkedIn</a></li>
+    </ul>
+</nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview">
+          <a href="#">
+            <h2 class="post-title">
+              Ma page d'accueil
+            </h2>
+          </a>
+<br>
+<br>
+          <a href="#about">
+            <h2 class="post-title">
+              à propos de moi
+            </h2>
+          </a>
+
+<br>
+<br>
+                  <a href="#skills">
+                    <h2 class="post-title">
+                      Mes compétences
+                    </h2>
+                  </a>
+</div>
+<br>
+<br>
+  <div>
+                  <a href="#experience">
+                    <h2 class="post-title">
+                      Mes Expériences
+                      </h2>
+                    </a>
+
+
+<br>
+<br>
+              <a href="#formation">
+                <h2 class="post-title">
+                  Ma formation
+                  </h2>
+                </a>
+
+<br>
+<br>
+            <a href="#Contact">
+              <h2 class="post-title">
+                Contactez-moi
+                </h2>
+              </a>
+
+
+        <hr>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <!-- Footer -->
+<?php include 'footer.php'; ?>
+    <div class="flechebas">
+        <a href="#about"><i class="fas fa-arrow-down"></i></a>
+    </div>
+  </footer>
+
+<hr>
+
+
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/perso1.jpeg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="page-heading">
+            <h1 id="about">A propos de moi</h1>
+            <span class="subheading">Voilà c'est moi.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <p>Je m'appelle Paul VORNIERE. J'ai 19ans je suis en BTS Systeme d'information des organisations. Je suis passionné par l'informatique certes , mais ma plus grande passion est l'automobile. J'adore les jeux vidéos , ils me permettent de relâcher la pression.</p>
+        <p>J'ai fais mes études en STMG ( Sciences et technologies du management et de la gestion ) au lycée Sainte-Ursule , j'ai eu mon Bac avec la mention assez bien.</p>
+        <p>Je suis sportif , j'ai fais 10ans de basket-ball et j'aime quasiment tout les sports ( sauf le Football si on peut appeler ça du sport )</p>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Footer -->
+<?php include 'footer.php' ?>
+<div class="flechebas">
+    <a href="#skills"><i class="fas fa-arrow-down"></i></a>
+</div>
+</footer>
+<hr>
+
+
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/code.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-heading">
+            <h1 id="skills">Mes compétences en Développement web </h1>
+            <h2 class="subheading"></h2>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+
+  <article class="competence">
+<h1>Compétences</h1>
+<h2>Développement</h2>
+<br>
+<label for="html">HTML/CSS</label>
+
+<progress id="html" max="100" value="70"> HTML/CSS </progress>
+
+<label for="php">PHP</label>
+
+<progress id="php" max="100" value="20"> PHP </progress>
+<br><br>
+<h2>Langues</h2>
+
+<label for="html">Anglais</label>
+
+<progress id="html" max="100" value="70"> Anglais </progress>
+
+<label for="php">Español</label>
+
+<progress id="php" max="100" value="30"> Espagnol </progress>
+
+  </article>
+
+
+
+  <!-- Footer -->
+<?php include 'footer.php' ?>
+    <div class="flechebas">
+        <a href="#experience"><i class="fas fa-arrow-down"></i></a>
+    </div>
+  </footer>
+
+<hr>
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/experience.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-heading">
+            <h1 id="experience">Expériences</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+
+  <article>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <h1>Expériences Profesionnelles</h1>
+<br><br>
+          <div class="poste1">
+            <h4>Employé communal</h4>
+            <h6>Commune d'EVRECY</h6>
+            <h6> 2 Juillet 2017 - 16 Juillet 2017</h6>
+            <h6 class="lieu">EVRECY</h6>
+          </div>
+<br><br>
+          <div class="poste2">
+            <h4>Stagière</h4>
+            <h6>Intermarché</h6>
+            <h6>2015</h6>
+            <h6 class="lieu">EVRECY</h6>
+          </div>
+<br><br>
+          <div class="poste3">
+            <h4>Stagière</h4>
+            <h6>Intermarché</h6>
+            <h6> 2016 </h6>
+            <h6 class="lieu">EVRECY</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <!-- Footer -->
+  <?php include 'footer.php' ?>
+    <div class="flechebas">
+        <a href="#formation"><i class="fas fa-arrow-down"></i></a>
+    </div>
+  </footer>
+<hr>
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/formation-pv.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-heading">
+            <h1 id="formation">Ma Formation</h1>
+            <h2 class="subheading"></h2>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+
+  <article>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+J'ai comme tout le monde suivie un début de formation standard jusqu'au collège.
+J'ai poursuivi dans un lycée technologique un Baccalauréat STMG auquel j'ai obtenu la mention assez bien.
+J'ai ensuite décidé de me lancer dans un DUT informatique auquel je n'est pas pu assisté plus de 3mois pour des raison médical.
+J'ai donc décidé de me réorienter vers un BTS Sio dans l'idée de faire du dévloppement.
+En commançant cette année de BTS mon idée a completement changée , le réseau me plaît beaucoup plus.
+
+        </div>
+      </div>
+    </div>
+  </article>
+
+
+  <!-- Footer -->
+<?php include 'footer.php' ?>
+    <div class="flechebas">
+        <a href="#contact"><i class="fas fa-arrow-down"></i></a>
+    </div>
+  </footer>
+<hr>
+
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/contact-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="page-heading">
+            <h1 id="contact">Contactez moi</h1>
+            <span class="subheading">Vous avez une question ?</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <p>Votre message sera envoyé à l'aide d'une adresse à usage unique (pour lutter contre le coronavirus)</p>
+
+
+        <form action="mail.php" method="post"  novalidate>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Nom/Pseudo</label>
+              <input type="text" class="form-control" placeholder="Nom" name="nom" required data-validation-required-message="Nom/Pseudo">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Adresse Email</label>
+              <input type="email" class="form-control" placeholder="E-Mail" name="email" required data-validation-required-message="Adresse Email">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Message</label>
+              <textarea rows="5" class="form-control" placeholder="Message" name="message" required data-validation-required-message="Votre message"></textarea>
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <br>
+<div class="g-recaptcha" data-sitekey="6LdLEdkZAAAAAAYEWcd-530mW5IyqYjJYv-W20oa" required></div>
+          <br>
+          <div id="success"></div>
+          <input type="submit" class="btn btn-primary" name="envoi" value="Envoyer">
+        </form>
+
+        <?php
+    if(isset($_POST['message'])){
+        $entete  = 'MIME-Version: 1.0' . "\r\n";
+        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $entete .= 'From: ' . $_POST['email'] . "\r\n";
+
+        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        <p><b>Nom : </b>' . $_POST['nom'] . '<br>
+        <b>Email : </b>' . $_POST['email'] . '<br>
+        <b>Message : </b>' . $_POST['message'] . '</p>';
+
+        $retour = mail('vorniere.paul@gmail.com', 'Envoi depuis page Contact', $message, $entete);
+        if($retour) {
+            echo '<p>Votre message a bien été envoyé.</p>';
+        }
+    }
+    ?>
+
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Footer -->
+<?php include 'footer.php' ?>
+    </div>
+    <div class="rgpd">
+        <b>Paul VORNIERE's web site</b>  n'utilise pas de cookies de traçage, et ne collecte aucune information sur votre navigation.
+    </div>
+  </footer>
+<hr>
+  <!-- Bootstrap core JavaScript -->
+  <script src="assets/jquery/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Script pour le Captcha -->
+   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</body>
+
+</html>
